dabd0b17c5c23065b8bd058281c1bff572e82e42
Joao <joaopaulodeoliveiramendonca@gmail.com>
2022-09-23 11:15:18 -0400

    up

--- Page.js
+++ Page.js
@@ -10,6 +10,7 @@ class Page {
     author = document.getElementById("author");
 
     ads = document.getElementById("ads");
+    localization = document.getElementById("localization");
 
     constructor(jsonPath) {
         fetch(jsonPath)
@@ -33,10 +34,10 @@ class Page {
                 }
 
                 this.categories.innerHTML = listOfCategories;
-
+                
                 this.adsTitle.innerHTML = data.adsTitle;
                 this.adsSubtitle.innerHTML = data.adsSubtitle;
-             
+
                 this.ads.classList.remove('text-white');
                 this.ads.classList.remove('bg-dark');
                 this.ads.style.backgroundColor = data.adsBackgroundColor;
@@ -46,15 +47,15 @@ class Page {
                 this.secondSubtitle.innerHTML = data.secondSubtitle;
 
                 var listOfProducts = "";
-            
+
                 for (var p = 0; p < data.products.length; p++) {
                     listOfProducts += '<div class="col-md-4">';
                     listOfProducts += '<div class="card mb-4 shadow-sm p-3">';
-                    listOfProducts += '<img class="card-img-top pb-2" src="'+data.products[p].imagePath+'">';
+                    listOfProducts += '<img class="card-img-top pb-2" src="' + data.products[p].imagePath + '">';
                     listOfProducts += '<div class="card-body border-top">';
-                    listOfProducts += '<h6 class="card-text">'+data.products[p].name+'</h6>';
-                    listOfProducts += '<h2 class="">'+data.products[p].price+'</h2>';
-                    listOfProducts += '<p class="card-text">'+data.products[p].payment+'</p>';
+                    listOfProducts += '<h6 class="card-text">' + data.products[p].name + '</h6>';
+                    listOfProducts += '<h2 class="">' + data.products[p].price + '</h2>';
+                    listOfProducts += '<p class="card-text">' + data.products[p].payment + '</p>';
                     listOfProducts += '<div class="d-flex justify-content-between align-items-center">';
                     listOfProducts += '<div class="btn-group">';
                     listOfProducts += '<button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>';
@@ -68,16 +69,34 @@ class Page {
 
                 products.innerHTML = listOfProducts;
 
-
-
-
                 this.author.innerHTML = data.systemSettings[0].author;
+
+                const url = 'https://viacep.com.br/ws/' + data.systemSettings[0].CEP + '/json';
+                const options = {
+                    method: "GET",
+                    mode: "cors",
+                    headers: {
+                        'content-type': 'application/json;charset=utf-8',
+                    }
+                }
+                fetch(url, options).then(
+                    response => response.json()
+                ).then(
+                    data => {
+                        this.localization.innerHTML = data.localidade +' - '+ data.uf;
+                    }
+                )
             });
 
-            $(window).on("load", function () {
-                document.body.style.display = 'unset';
-            });
+
+
+
+        $(window).on("load", function () {
+            document.body.style.display = 'unset';
+        });
+
     }
+
 }
 
 new Page("database.json");
